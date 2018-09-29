316bb1a34a68f5fec22a87329296be6ef9490b36
thatguythatsincharge <36935809+thatguythatsincharge@users.noreply.github.com>
2018-09-29 10:30:15 -0700

    added a little bit

--- server.js
+++ server.js
@@ -7,3 +7,17 @@ app.use(express.static('public'));
 app.get('/', function(req, res) {
 	res.render('home');
 });
+
+port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+	if (port= process.env.PORT) {
+		console.log('The application is running on the port');
+	}else if (port=3000) {
+		console.log("The application is running on port 3000");
+	}else{
+		console.log("The application is not running on process.env.PORT or port 3000 - Go check whats wrong");
+	}
+});
